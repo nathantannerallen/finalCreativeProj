28772898ae4d0e19cabd9a0416d9482bc99cdd37
EC2 Default User <ec2-user@ip-172-31-87-2.ec2.internal>
2019-12-10 18:15:55 +0000

    Updated, combined, and added with previous creative projects

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,21 +1,82 @@
 /*global axios */
 /*global Vue */
+const formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2
+});
+
 var app = new Vue({
   el: '#voting',
   data: {
     products: [],
     product: '',
     price: '',
-    url:'',
+    url: '',
     ballot: [],
+    done: false,
+    searchText: '',
+    currentTicker: {
+      price: '',
+      revenue: '',
+      revenueGrowth: '',
+      COGS: '',
+      NetIncome: '',
+      NetProfitMargin: '',
+      EPS: '',
+    },
   },
   created: function() {
     this.getall();
   },
   methods: {
+    async grabStockInfo() {
+      console.log("inStockInfo");
+      await this.grabPriceInfo();
+      await this.grabIncomeInfo();
+      this.done = true;
+    },
+    async grabPriceInfo() {
+      console.log("inPriceInfo");
+      try {
+        const url = "https://financialmodelingprep.com/api/v3/stock/real-time-price/" + this.searchText;
+        const response = await axios.get(url);
+        console.log(response);
+        this.currentTicker.price = formatter.format(response.data.price)
+        console.log(this.currentTicker.price);
+      }
+      catch (error) {
+        console.log(error);
+      }
+    },
+    async grabIncomeInfo() {
+      console.log("inIncomeInfo");
+      try {
+        const url = "https://financialmodelingprep.com/api/v3/financials/income-statement/" + this.searchText;
+        const response = await axios.get(url);
+        console.log(response);
+        this.currentTicker.revenue = formatter.format(response.data.financials[0].Revenue)
+        this.currentTicker.revenueGrowth = parseFloat(response.data.financials[0]["Revenue Growth"] * 100).toFixed(2) + "%"
+        this.currentTicker.COGS = formatter.format(response.data.financials[0]["Cost of Revenue"])
+        this.currentTicker.NetIncome = formatter.format(response.data.financials[0]["Net Income"])
+        this.currentTicker.NetProfitMargin = parseFloat(response.data.financials[0]["Net Profit Margin"] * 100).toFixed(2) + "%"
+        this.currentTicker.EPS = formatter.format(response.data.financials[0].EPS)
+
+        console.log(this.currentTicker.revenue);
+        console.log(this.currentTicker.revenueGrowth);
+        console.log(this.currentTicker.COGS);
+        console.log(this.currentTicker.NetIncome);
+        console.log(this.currentTicker.NetProfitMargin);
+        console.log(this.currentTicker.EPS);
+
+      }
+      catch (error) {
+        console.log(error);
+      }
+    },
     addProduct() {
       console.log("In add Product")
-      var url = "http://web.nathantannerallen.com:4204/voting";
+      var url = "http://web.nathantannerallen.com:4202/voting";
       axios.post(url, {
           Name: this.product,
           price: this.price,
@@ -36,7 +97,7 @@ var app = new Vue({
     deleteproduct(product) {
       var index = this.products.indexOf(product);
       if (index > -1) {
-        var url = "http://web.nathantannerallen.com:4204/voting/" + product._id;
+        var url = "http://web.nathantannerallen.com:4202/voting/" + product._id;
         axios.delete(url)
           .then(response => {
             console.log(response.data.votes);
@@ -49,7 +110,7 @@ var app = new Vue({
       }
     },
     upvote(product) {
-      var url = "http://web.nathantannerallen.com:4204/voting/" + product._id + "/upvote";
+      var url = "http://web.nathantannerallen.com:4202/voting/" + product._id + "/upvote";
       console.log("upvote URL " + url);
       axios.put(url)
         .then(response => {
@@ -62,7 +123,7 @@ var app = new Vue({
     },
     async getall() {
       console.log("get all");
-      var url = "http://web.nathantannerallen.com:4204/voting"; // This is the route we set up in index.js
+      var url = "http://web.nathantannerallen.com:4202/voting"; // This is the route we set up in index.js
       try {
         let response = await axios.get(url);
         this.products = response.data; // Assign array to returned response
@@ -75,7 +136,7 @@ var app = new Vue({
     },
     dovote() {
       console.log("In Dovote");
-      for(var product of this.products) {
+      for (var product of this.products) {
         if (product.selected) {
           this.upvote(product);
           this.ballot.push(product);
@@ -84,4 +145,3 @@ var app = new Vue({
     },
   }
 });
-
